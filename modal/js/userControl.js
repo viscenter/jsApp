d9f588290264b1ac2ab8e3554d2949af00aec1f5
John Broadbent <john.broadbent@uky.edu>
2013-08-15 14:38:55 -0400

    cleanup

--- modal/js/userControl.js
+++ modal/js/userControl.js
@@ -1,12 +1,13 @@
 "use strict";
 
-        //use the code below for real (furman CITE server)
-	
-	setUpObj_Layers();      //Running the helper function to set up the CITE objects
+	//Enable these for pulling from CITE server
+
+	//setUpObj_Layers();      //Running the helper function to set up the CITE objects
 	//setUpThumbNail();//Same as above for smaller images
 	
 	
-	
+	//Comment out when enabling CITE server
+    //Setup local layers
 	setUpObj_Layers_local(); //This make a fake CITE object that uses local files
 	                         //This is pretty hacky. Now we can use a global object called localCoppCITE
 	
@@ -19,33 +20,55 @@
 	chad_cite_Layers =   localCopyCITE;//This line over writes the cite object with the local fake cite objct
 	             
 
-	
+	//All possible layers
 	var totalListOfLayers = new Array(); //We only create this once and keep this array till the end
 	                                 //It will hold a max of 5 images for our uses
-     
-        var currentListOfLayers = new Array(); //We only create this once and keep this array till the end
+    //All enabled layers that we are currently viewing
+    var currentListOfLayers = new Array(); //We only create this once and keep this array till the end
                                         //This is holds the images that we are currently viewing. 
                                         //It's elements will always be a subset of the totalListOfImages
   
   
 
 
+    //This value holds the state of the zoom for all images
 	var imageSize = 888;//This is bad form
-  			     //This value holds the state of the zoom for all images
   	
   	
-  	
+  	//Zoom limits
   	var maxZoomSize = imageSize /5;  
   	var minZoomSize = imageSize * 10 ;
   	
-  	
+  	//Scaling factor for zoom
   	var zoomRatio = 6/5;
   					
+    //Dimensional properties			
 	var scale = 1.0;
     var imageHeight = -1;
     var imageWidth =-1;
-    
 
+    //amount we move when we pan
+    var panDistance = 10;
+
+    //The height of the upperheader of the webPage
+    var headerSize = 50;
+    
+    //scroll sensitivity, how long does a user have to scroll before the message is send
+    var scrollThresh = 10;
+
+    //defaultText, this is the text that will displayed when nothing is selected in the viewer
+    var descriptionDefaultText = "<p><strong>Description:</strong> Above is the data set of the Chad Gospels that were imaged with permission from the Lichfield Cathedral in England. Select the layers you wish to view—either on its own or in comparison with another—by clicking on the thumbnail.</p></div>";
+
+    //These are the discriptions for the thumbnails
+    var descr[];
+    descr[0]="The high-resolution composite of images taken in 2010 at the Lichfield Cathedral. This page contains Matthew 17:22-Matthew 18:3"
+    descr[1]="The composite of images taken in different light spectrums that have been processed by performing mathematical equations to the individual pixels to render a new image. This page contains Matthew 17:22-Matthew 18:3.    "  ; 
+    descr[2] ="The set of images taken of the Chad Gospels in 2003. This page contains Matthew 17:22-Matthew 18:3."; 
+    descr[3]="The set of images taken of the Chad Gospels in 1962. This page contains Matthew 17:22-Matthew 18:3.  "; 
+    descr[4] ="The set of images taken of the Chad Gospels in 1929. This page contains Matthew 17:22-Matthew 18:3.";
+
+
+//Creating index for layers
   var layers={
 	"Chad-RGB-141":0,
 	"Chad-Intc-141":1,
@@ -55,16 +78,17 @@
   }
 
 
-//html was here
-//////////////////////////////////////////////////////////////////////////////
 
+//Request page from CITE server
+//chad_cite_Layers.setPage(79);
 
-
-chad_cite_Layers.setPage(79);
+//populate list totalListOfLayer
 getlayers();
 
-setThumb()
+//populate list of thumbs from the HTML
+setThumb();
 
+/* Functions for changing pages
 function prev()
 { 
 	chad_cite_Layers.prevPage();
@@ -80,6 +104,7 @@ function next()
     getlayers();
 
 }
+*/
 
 
 
@@ -139,18 +164,17 @@ function smaller()
 
 
 
-//The 4 functions below are pan control functions. They all call the the pan(,) function but with different
-//arguments. These functions are not currently in use. The functions simulate the dragging movments. 
-//They were used when we had buttons to pan, but now. We use drag as a sole means a panning
+//The 4 functions below are pan control functions. They all call the pan() function but with different
+//arguments. The functions simulate the dragging movements. Mapped to the arrow keys by whenKeyPressed().
 function panUp()
 {
      var t = document.getElementById("myFrame").offsetHeight;
-     pan(0,-t/10);
+     pan(0,-t/panDistance);
 }
 function panDown()
 {
      var t = document.getElementById("myFrame").offsetHeight;
-     pan(0,t/10);
+     pan(0,t/panDistance);
 }
 
 
@@ -158,13 +182,13 @@ function panDown()
 function panRight()
 {
      var t = document.getElementById("myFrame").offsetWidth;
-     pan(t/10,0);
+     pan(t/panDistance,0);
 }
 
 function panLeft()
 {
      var t = document.getElementById("myFrame").offsetWidth;
-     pan(-t/10,0);
+     pan(-t/panDistance,0);
 }
 
 
@@ -181,6 +205,8 @@ function pan(deltaX,deltaY)
 //GetLayers uses the cite object called chad_cite_Layers, to 
 //generate a an array of all the layers called totalListOfLayers.
 //This could lead to great load times.
+//FIX: Needs to clear image objects when changing pages.
+
 function getlayers()
 {
 	var i = 0;
@@ -189,8 +215,8 @@ function getlayers()
 		var temp = new Image();
 		chad_cite_Layers.setLayer(name);
 		temp.onload = function() { 
-			             //This is the image lisener, a pretty standard js desgine pattern,
-			             //As soon as the image is loaded the code in the lisener will run.
+	                    	//This is the image listener. Waits until the image is loaded, then gets the dimensions if not yet defined
+       						 //Without this, Javascript tries to get dimensions before image is loaded
 					    fakeThumb.setLayer(this.id);
 					    document.getElementsByClassName("thumb")[parseInt(this.getAttribute("number"))].children[0].src = fakeThumb.fake() //= this.src;
 		                            if(imageHeight === -1)
@@ -199,17 +225,19 @@ function getlayers()
 		  								  imageWidth = parseInt(totalListOfLayers[this.getAttribute("number")].width,0)* 1.5;
 										 
 		}
-		//console.log(chad_cite_Layers.fake());
+		//Write to the console where the program is looking for the image
 		temp.src =chad_cite_Layers.fake();
 
-        	temp.setAttribute("number",i.toString())
+		//Setup all attributes of this image object
+        temp.setAttribute("number",i.toString())
 		temp.name ="SingleMainImage";
-		//temp.id = layerNames[counter];
 		temp.id = name;
 		temp.style.position = 'absolute';
 		temp.width = "592";
 		temp.height ="789";
+		//Write to the image source to the console
 		console.log(temp.src);
+		//Push temp object to the totalListOfLayers
 		totalListOfLayers.push(temp);   
 		i = i +1;
 		
@@ -230,42 +258,36 @@ function getlayers()
 
 
 
+
+
 //displayLayers()
 //-----------------------
 //This function displays the images that are in the currentListOfLayers.
-//To do this it removes all images that were being displayed and then, iterates 
-//the currentListOfLayers array.
+//To do this it removes all images that were being displayed and then iterates 
+//the currentListOfLayers array. It essentially redraws everything.
 function displayLayers()
 {
 	
-	
+    //Clears out old layer images.
 	while(document.getElementById("myFrame").contentDocument.getElementById("imageDiv").hasChildNodes() == true)
 	{
 		document.getElementById("myFrame").contentDocument.getElementById("imageDiv").removeChild(document.getElementById("myFrame").contentDocument.getElementById("imageDiv").childNodes[0]);
 	}
 	
-	//while(window.frames['myFrame'].document.getElementById("imageDiv").hasChildNodes() == true)
-	//{
-	//	window.frames['myFrame'].document.getElementById("imageDiv").removeChild(window.frames['myFrame'].document.getElementById("imageDiv").childNodes[0]);
-	//}
-
+	//Iterates through all selected layers, sets their updated opacity, and displays
 	var counter = 0;
 	while(counter < currentListOfLayers.length)
 	{
 		currentListOfLayers[counter].align="center";
-		// =  currentListOfLayers[counter].width * scale ;//imageSize
-		//= currentListOfLayers[counter].height *scale ;//imageSize; 
 		
 		currentListOfLayers[counter].width  =imageWidth;
 		currentListOfLayers[counter].height  = imageHeight;
 		currentListOfLayers[counter].style.opacity = 1.0/(counter+1) ;//currentListOfLayers.length;
-		//(window.frames['myFrame'].document.getElementById("imageDiv")).appendChild(currentListOfLayers[counter]);
 		document.getElementById("myFrame").contentDocument.getElementById("imageDiv").appendChild(currentListOfLayers[counter]);
 		counter = counter + 1;
 	}
 	
 }
-
 
 
 
@@ -285,6 +307,8 @@ function updateLayers(layerValue)
 	 	console.log("the value == true")
 	 	layerValue.checked = false;
 	 	layerValue.setAttribute("checked","false");
+
+	 	//Sets thumb class to selected for CSS
 	    var pic = layerValue.children[0];
 	    pic.className = "selected";
 
@@ -294,6 +318,7 @@ function updateLayers(layerValue)
 		console.log("the value was set to false");
 		layerValue.setAttribute("checked","true");
 		
+		//Sets thumb class to unselected for CSS
     	var pic = layerValue.children[0]
 		pic.className = "unselected"
     
@@ -303,14 +328,15 @@ function updateLayers(layerValue)
 		{
 			if( layerValue.id == currentListOfLayers[i].id)
 			{
+				//Move unchecked item to end of array and remove it
 				currentListOfLayers[i]= currentListOfLayers[currentListOfLayers.length - 1];
 				currentListOfLayers.pop();		
 			}
 		i = i + 1;
 		}
 	}
-	
-	
+
+	//Render changes in the display area
 	displayLayers();
 	
 }
@@ -330,7 +356,7 @@ function whenFrameLoads()
 	var fWidth  = document.getElementById("myFrame").offsetWidth/2;
 
 	document.getElementById("myFrame").contentDocument.getElementById("draggable").style.left =fWidth - imageSize/2;
-	document.getElementById("myFrame").contentDocument.getElementById("draggable").style.top =50; //Size of the header bar above the image
+	document.getElementById("myFrame").contentDocument.getElementById("draggable").style.top = headerSize; // 50 Size of the header bar above the image
 	
         clearText();
 
@@ -345,24 +371,24 @@ function whenThumbMouseOver(that)
 }
 function clearText()
 {
- document.getElementById("description").innerHTML =" <p><strong>Description:</strong> Above is the data set of the Chad Gospels that were imaged with permission from the Lichfield Cathedral in England. Select the layers you wish to view—either on its own or in comparison with another—by clicking on the thumbnail.</p></div>";
+ document.getElementById("description").innerHTML =descriptionDefaultText;
 }
 
 
-//setThumb()
-//There are several thumb nails at the top of the webpage and the correct image must be loaded into them.
+//There are several thumbnails at the top of the webpage and the correct image must be loaded into them.
 function upDateThumb()
 {
-//alert("setThumb")
+	/* DOUBLE CHECK: Used during page changes?
 	var listOfThumb = document.getElementsByClassName("thumb");
 	for(var i = 0; i<listOfThumb.length;i++)
 	{
 
-	}
+	}*/
 
+     //Get the thumb elements from the HTML
      var y = document.getElementsByClassName("thumb");
 
-     //Make the first Image selected
+     //Auto-select the first thumbnail if nothing is selected
      if(currentListOfLayers.length == 0)
      {
        var x = document.getElementsByClassName("unselected");
@@ -371,15 +397,13 @@ function upDateThumb()
        currentListOfLayers.push(totalListOfLayers[0]);
        displayLayers();
     }
+        //Links thumbnail to a description of that layer. Used by whenThumbMouseOver()
     var listOfDest=[];
-    listOfDest[0]="The high-resolution composite of images taken in 2010 at the Lichfield Cathedral. This page contains Matthew 17:22-Matthew 18:3"
-    listOfDest[1]="The composite of images taken in different light spectrums that have been processed by performing mathematical equations to the individual pixels to render a new image. This page contains Matthew 17:22-Matthew 18:3.    "  ; 
-    listOfDest[2] ="The set of images taken of the Chad Gospels in 2003. This page contains Matthew 17:22-Matthew 18:3."; 
-    listOfDest[3]="The set of images taken of the Chad Gospels in 1962. This page contains Matthew 17:22-Matthew 18:3.  "; 
-    listOfDest[4] ="The set of images taken of the Chad Gospels in 1929. This page contains Matthew 17:22-Matthew 18:3.";
-    //listOfDest[5] ="n/a";  
-    //listOfDest[6]="n/a";
-    //listOfDest[7]="n/a";
+    listOfDest[0] = descr[0];//"The high-resolution composite of images taken in 2010 at the Lichfield Cathedral. This page contains Matthew 17:22-Matthew 18:3"
+    listOfDest[1] = descr[1];// "The composite of images taken in different light spectrums that have been processed by performing mathematical equations to the individual pixels to render a new image. This page contains Matthew 17:22-Matthew 18:3.    "  ; 
+    listOfDest[2] = descr[2];//"The set of images taken of the Chad Gospels in 2003. This page contains Matthew 17:22-Matthew 18:3."; 
+    listOfDest[3] = descr[3];//"The set of images taken of the Chad Gospels in 1962. This page contains Matthew 17:22-Matthew 18:3.  "; 
+    listOfDest[4] = descr[4];//"The set of images taken of the Chad Gospels in 1929. This page contains Matthew 17:22-Matthew 18:3.";
 
     //Adding the descriptions
     for(var i = 0; i<y.length;i++)
@@ -391,18 +415,18 @@ function upDateThumb()
 
 }
 
-function whenScrolled()
-{
- alert("scroll event detected! " + window.pageXOffset + " " + window.pageYOffset);
- // note: you can use window.innerWidth and window.innerHeight to access the width and height of the viewing area
-}
+
+
+//Mouse wheel zoom functionality
+//Google Chrome specific
+//Changing these values changes how "quickly" one zooms
 
 function whenMouseWheel(event)
 {
-        if( event.wheelDeltaY > 10){
+        if( event.wheelDeltaY > scrollThresh){
 			bigger();       
 	     }
-        if(event.wheelDeltaY <-10 ){
+        if(event.wheelDeltaY <- scrollThresh ){
              smaller();
     	}
     	 
